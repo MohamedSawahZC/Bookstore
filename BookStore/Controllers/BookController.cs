9d42ce6c03dce7b1d1514519bb307e75a145a2c7
Mohamed Sawah <61028956+MohamedSawahZC@users.noreply.github.com>
2022-12-09 20:02:13 +0200

    Finish Author and book and create and view book model

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,108 @@
+﻿using BookStore.Models;
+using BookStore.Models.Repositories;
+using BookStore.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookStoreRepository<Book> bookRepository;
+        private readonly IBookStoreRepository<Author> authorRepoosity;
+        public BookController(IBookStoreRepository<Book> bookRepository,IBookStoreRepository<Author> authorRepoosity)
+        {
+            this.bookRepository = bookRepository;
+            this.authorRepoosity = authorRepoosity;
+
+        }
+        // GET: BookController
+        public ActionResult Index()
+        {
+            var books = bookRepository.List();
+            return View(books);
+        }
+
+        // GET: BookController/Details/5
+        public ActionResult Details(int id)
+        {
+            var book = bookRepository.Find(id);
+            return View(book);
+        }
+
+        // GET: BookController/Create
+        public ActionResult Create()
+        {
+            var model = new BookAuthorViewModel
+            {
+                Authors = authorRepoosity.List().ToList()
+            };
+            return View(model);
+        }
+
+        // POST: BookController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BookAuthorViewModel model)
+        {
+            try
+            {
+                Book book = new Book
+                {
+                    Id = model.BookId,
+                    Title = model.Title,
+                    Description = model.Description,
+                    Author = authorRepoosity.Find(model.AuthorId)
+                };
+                bookRepository.Add(book);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BookController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BookController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
